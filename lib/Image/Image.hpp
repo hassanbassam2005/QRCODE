1a4fab5df4bdb34559c5c77ce5c1c8bc904048f6
hassanbassam2005 <HASSANBASSAM2005@GMAIL.COM>
2024-11-29 01:16:15 +0200

    adding comments

--- lib/Image/Image.hpp
+++ lib/Image/Image.hpp
@@ -26,8 +26,21 @@ namespace IMAGE
 
 	struct DEFAULT
 	{
+		/**
+		* @brief Prints the QR code with default colors (black and white).
+		* @param qr The QR code object to be printed.
+		*/
 		static void PRINT_QR(const QR::QRCODE& qr);
-		static void PRINT_QR(const QR::QRCODE& qr, int r, int g,int b);
+
+		/**
+		 * @brief Prints the QR code with a custom color specified by the RGB values.
+		 * @param qr The QR code object to be printed.
+		 * @param r The red component of the custom color (0-255).
+		 * @param g The green component of the custom color (0-255).
+		 * @param b The blue component of the custom color (0-255).
+		 */
+		static void PRINT_QR(const QR::QRCODE& qr, int r, int g, int b);
+
 	};
 }
 
